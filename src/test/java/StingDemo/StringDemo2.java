7615cb4fa524f75b70ee2d2ce279d1acca619ec9
Priyanka_Deb <debpriyanka1992@gmail.com>
2019-04-26 14:19:12 +0530

    hghgh

---
+++ src/test/java/StingDemo/StringDemo2.java
@@ -0,0 +1,11 @@
+package StingDemo;
+
+public class StringDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1="fhfhfh";
+System.out.println(s1);
+	}
+
+}
